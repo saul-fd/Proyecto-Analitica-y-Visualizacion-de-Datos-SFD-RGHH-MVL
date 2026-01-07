7b500d9d2cb3decce6509fffe82ea1880a3b47ac
saul-fd <132414518+saul-fd@users.noreply.github.com>
2026-01-07 00:13:53 -0600

    Add new views for statistics and correlation analysis

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from views.home import show_home
 from views.lineas import show_lineas
+from views.correlacion import show_correlacion
+from views.estadisticas import show_estadisticas
+from views.temporal import show_temporal 
 
 # --- CONFIGURACIÓN GLOBAL ---
 # Se define una sola vez al principio de la app
@@ -27,3 +30,10 @@ if pagina == "Inicio (General)":
     show_home()
 elif pagina == "Detalle por Línea":
     show_lineas()
+elif pagina == "Estadística Descriptiva":
+    show_estadisticas()
+elif pagina == "Correlación y PCA":
+    show_correlacion()
+elif pagina == "Análisis Espectral (Fourier)": 
+    show_temporal()
+
